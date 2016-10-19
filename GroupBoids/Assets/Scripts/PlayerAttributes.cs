d5011697fbe14748f3a1b10efee45cc3c1ba5ccf
Rory Vaughn <Rvaughn2525@gmail.com>
2016-10-19 16:21:37 -0500

    Revert "invunerabilty"
    This reverts commit 253906d91d3ed7f9ff81fd0c9f37e2b56dab0e58.

--- GroupBoids/Assets/Scripts/PlayerAttributes.cs
+++ GroupBoids/Assets/Scripts/PlayerAttributes.cs
@@ -9,40 +9,14 @@ public class PlayerAttributes : MonoBehaviour {
     public int health;
     public string gameOverScene;
     public Slider healthSlider;
-    bool mesh = true;
-    float resetTime = 20.0f;
-    float currenttime;
-
+	
     void Start()
     {
         hitSource = gameObject.GetComponent<AudioSource>();
     }
-    void reset()
-    {
-        currenttime = resetTime;
-        mesh = false;
-    }
-    // Update is called once per frame
-    void Update () {
 
-        if (currenttime <= 1.2)
-        {
-            mesh = true;
-        }
-        if (mesh == false)
-        {
-            currenttime -= 0.2f;
-            int cTime = (int)currenttime;
-            float i = (float)cTime;
-            if (i % 2 == 0)
-            {
-                transform.parent.gameObject.GetComponent<MeshRenderer>().enabled = false;
-            }
-            else
-            {
-                transform.parent.gameObject.GetComponent<MeshRenderer>().enabled = true;
-            }
-        }
+	// Update is called once per frame
+	void Update () {
 
         healthSlider.value = health;
 
@@ -56,13 +30,9 @@ public class PlayerAttributes : MonoBehaviour {
     {
         if (other.gameObject.name == "Boid(Clone)") //Collision
         {
-            if (mesh == true)
-            {
-                hitSource.Play();
-                health--;
-                reset();
-                Destroy(other.gameObject);
-            }  
+            hitSource.Play();
+            health--;           
+            Destroy(other.gameObject);         
         }
     }
 }
